a99b3b43739390d395faa7e24fcce1e20078c42f
davidukim <39535370+davidukim@users.noreply.github.com>
2024-06-18 13:25:24 -0400

    Update longestSubstringNoDuplicate.java

--- SlidingWindow/longestSubstringNoDuplicate.java
+++ SlidingWindow/longestSubstringNoDuplicate.java
@@ -7,16 +7,27 @@ class Solution {
         int right = 0;
         int maxLength = 0;
         boolean[] checkExist = new boolean[256];    // array to check for ASCII character's existence 
-
-        while (right < s.length){   //until right exceeds last index position
-            if ( checkExist[s.charAt(right)] ){ // if character is checked true(exist) in the array
-                while (checkExsit[s.charAt(right)]){
-
+        
+        System.out.println(s);
+        while (right < s.length()){   //until right exceeds last index position
+            System.out.println("Beginning outer loop..");
+            if ( checkExist[s.charAt(right)] ){ // if char exists in the array
+                System.out.println(s.charAt(right) + "exists in the array");
+                while (checkExist[s.charAt(right)]){ // while char exists
+                    System.out.println("Setting " + s.charAt(left) + " to false");
+                    checkExist[s.charAt(left)] = false;
+                    left ++;                         // move left to + 1
+                    
                 }
-            } else{ // if character is unique
+            } else{ // if character doesn't exist in the array
+                System.out.println(s.charAt(right) + " doesn't exist in the array");
+                System.out.println("Setting " + s.charAt(right) + " to true");
                 checkExist[s.charAt(right)] = true;
             }
+            maxLength = Math.max(maxLength, right-left+1);
+            checkExist[s.charAt(right)] = true;
             right ++;
         }
+        return maxLength;
     }
 }
